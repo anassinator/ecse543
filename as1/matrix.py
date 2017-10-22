ebe7c6c17d0b0dfbb4d5c0ebae9f5480bc31989f
Anass Al <mr@anassinator.com>
2017-10-22 18:54:00 -0400

    Add preliminary SparseMatrix2D implementation

--- as1/matrix.py
+++ as1/matrix.py
@@ -1,5 +1,6 @@
 import copy
 import operator
+from collections import defaultdict
 
 
 class Matrix2D(object):
@@ -61,7 +62,7 @@ class Matrix2D(object):
             return self._arr[idx]
 
         if isinstance(idx, slice):
-            return Matrix2D(self._arr[idx])
+            return self.__class__(self._arr[idx])
 
         if isinstance(idx, tuple):
             x, y = idx
@@ -75,7 +76,7 @@ class Matrix2D(object):
                         m.append([a[y]])
                     else:
                         m.append(a[y])
-                return Matrix2D(m)
+                return self.__class__(m)
 
         raise NotImplementedError()
 
@@ -140,6 +141,9 @@ class Matrix2D(object):
     def __mod__(self, other):
         return self.__operate(other, operator.mod)
 
+    def __neg__(self):
+        return self.map(lambda x: -x)
+
     def __len__(self):
         return len(self._arr)
 
@@ -183,7 +187,7 @@ class Matrix2D(object):
                 raise ValueError("incompatible shape: {} and {}".format(
                     self.shape, other.shape))
 
-            m = Matrix2D.zeros(self.rows, other.cols)
+            m = self.__class__.zeros(self.rows, other.cols)
             for x in range(m.rows):
                 for y in range(m.cols):
                     m[x, y] = sum(
@@ -200,32 +204,184 @@ class Matrix2D(object):
     def map(self, f):
         m = self.clone()
         m = list(map(lambda row: list(map(f, row)), m))
-        return Matrix2D(m)
+        return self.__class__(m)
 
     def transpose(self):
-        m = Matrix2D.zeros(self.cols, self.rows)
+        m = self.__class__.zeros(self.cols, self.rows)
         for x in range(m.rows):
             for y in range(m.cols):
                 m[x, y] = self[y, x]
         return m
 
     def clone(self):
-        return Matrix2D(copy.deepcopy(self._arr), self._rows, self._cols)
+        return self.__class__(copy.deepcopy(self._arr), self._rows, self._cols)
 
-    @staticmethod
-    def zeros(rows, cols, dtype=float):
+    @classmethod
+    def zeros(cls, rows, cols, dtype=float):
         arr = [[dtype(0.) for _ in range(cols)] for _ in range(rows)]
-        return Matrix2D(arr, rows, cols)
+        return cls(arr, rows, cols)
 
-    @staticmethod
-    def ones(rows, cols, dtype=float):
+    @classmethod
+    def ones(cls, rows, cols, dtype=float):
         arr = [[dtype(1.) for _ in range(cols)] for _ in range(rows)]
-        return Matrix2D(arr, rows, cols)
+        return cls(arr, rows, cols)
 
-    @staticmethod
-    def eye(n, dtype=float):
+    @classmethod
+    def eye(cls, n, dtype=float):
         arr = [
             [dtype(1.) if i == j else dtype(0.) for i in range(n)]
             for j in range(n)
         ]
-        return Matrix2D(arr, n, n)
+        return cls(arr, n, n)
+
+
+class SparseMatrix2D(Matrix2D):
+
+    def __init__(self, arr, rows=None, cols=None, dtype=None):
+        if not isinstance(arr, list):
+            raise ValueError("incompatible type: {}".format(type(arr)))
+
+        if len(arr) == 0:
+            raise ValueError("incompatible size: 0")
+
+        if not isinstance(arr[0], list):
+            arr = [arr]
+
+        self._arr = arr
+
+        if rows is not None:
+            self._rows = rows
+        else:
+            self._rows = len(arr)
+
+        if cols is not None:
+            self._cols = cols
+        else:
+            self._cols = len(arr[0])
+
+        if dtype is not None:
+            self._dtype = dtype
+            if type(arr[0][0]) != dtype:
+                self._arr = self.map(dtype)._arr
+        else:
+            self._dtype = type(arr[0][0])
+
+        self._elements = defaultdict(lambda: defaultdict(self._dtype))
+        for x in range(self._rows):
+            for y in range(self._cols):
+                if arr[x][y]:
+                    self._elements[x][y] = arr[x][y]
+
+    def __setitem__(self, idx, value):
+        super().__setitem__(idx, value)
+
+        if isinstance(idx, int):
+            if value == 0:
+                if idx in self._elements:
+                    del self._elements[idx]
+            else:
+                self._elements[idx] = {
+                    i: self._arr[idx][i]
+                    for i in self._cols
+                    if self._arr[idx][i] != 0
+                }
+
+        if isinstance(idx, slice):
+            for x in slice_to_range(idx):
+                self._elements[x] = {
+                    y: self._arr[x][y]
+                    for y in self._cols
+                    if self._arr[x][y] != 0
+                }
+
+        if isinstance(idx, tuple):
+            x, y = idx
+            if isinstance(x, int):
+                if isinstance(y, int):
+                    self._elements[x][y] = value
+                    if value == 0:
+                        del self._elements[x][y]
+                    return
+
+                self._elements[x] = {
+                    y: self._arr[x][y]
+                    for y in self._cols
+                    if self._arr[x][y] != 0
+                }
+                return
+
+            for x in slice_to_range(idx):
+                self._elements[x] = {
+                    y: self._arr[x][y]
+                    for y in self._cols
+                    if self._arr[x][y] != 0
+                }
+
+    def __repr__(self):
+        return "SparseMatrix2D({})".format(self._arr.__repr__())
+
+    def __operate(self, other, op):
+        if isinstance(other, SparseMatrix2D):
+            seen = set()
+            for x in self._elements:
+                for y in self._elements[x]:
+                    self._arr[x][y] = op(self._arr[x][y], other[x][y])
+                    self._elements[x][y] = self._arr[x][y]
+                    seen.add((x, y))
+
+            for x in other._elements:
+                for y in other._elements[x]:
+                    if (x, y) in seen:
+                        continue
+                    self._arr[x][y] = op(self._arr[x][y], other[x][y])
+                    self._elements[x][y] = self._arr[x][y]
+
+        return super().__operate(other, op)
+
+    def dot(self, other):
+        if isinstance(other, Matrix2D):
+            if self.cols != other.rows:
+                raise ValueError("incompatible shape: {} and {}".format(
+                    self.shape, other.shape))
+
+            if False and isinstance(other, SparseMatrix2D):
+                m = SparseMatrix2D.zeros(self.rows, other.cols)
+                for x in other._elements:
+                    row = self._elements[x]
+                    for y in other._elements[x]:
+                        for _x in row:
+                            m[x, y] += row[_x] * other._arr[_x][y]
+            else:
+                m = Matrix2D.zeros(self.rows, other.cols)
+                for x in range(m.rows):
+                    row = self._elements[x]
+                    for y in range(m.cols):
+                        for _x in row:
+                            m[x, y] += row[_x] * other[_x, y]
+            return m
+
+        if isinstance(other, list):
+            m = Matrix2D([other]).T
+            return self.dot(m)
+
+        raise NotImplementedError()
+
+    def map(self, f):
+        zero_value = f(0.)
+        if zero_value == 0:
+            m = self.clone()
+            for x in m._elements:
+                for y in m._elements[x]:
+                    value = f(m._elements[x][y])
+                    m._elements[x][y] = value
+                    m._arr[x][y] = value
+            return m
+        else:
+            return super().map(f)
+
+
+def slice_to_range(s):
+    start = s.start if s.start is not None else 0
+    stop = s.stop if s.stop is not None else -1
+    step = s.step if s.start is not None else 1
+    return range(start, stop, step)
